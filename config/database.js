6879e3a60d347781f3d74cb126fd17099dc68cf1
Thomas Visintainer <tvisintainer@Tjslaserinnovations.onmicrosoft.com>
2018-05-18 15:38:57 -0500

    first

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://testing:testing1908@ds121950.mlab.com:21950/local_library', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
+
